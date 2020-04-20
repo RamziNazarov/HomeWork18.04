b78e7f0b322cdb74b7df2ddd388cd9a543ad1259
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-20 17:50:59 +0300

    create method Delete

--- Program.cs
+++ Program.cs
@@ -19,6 +19,20 @@ namespace HomeWork18._04
             T result = (dynamic)firstEl * (dynamic)secondEl;
             return result;
         }
+        static T Delete<T>(T firstEl, T secondEl)
+        {
+            T result;
+            if(secondEl == (dynamic)0)
+            {
+                result = (dynamic)0;
+                return result;
+            }
+            else
+            {
+                result = (dynamic)firstEl / (dynamic)secondEl;
+                return result;
+            }
+        }
         static void Main()
         {
 
